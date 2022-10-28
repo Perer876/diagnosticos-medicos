e7470de8ebee21b9e3c4ee6f0eb7254165c14eb9
Priscila González <psarahi.gonzalez@alumnos.udg.mx>
2022-10-27 19:29:32 -0500

    Creacion de los modelos

---
+++ app/Models/Enfermedad.php
@@ -0,0 +1,31 @@
+<?php
+
+namespace App\Models;
+
+use Illuminate\Database\Eloquent\Factories\HasFactory;
+use Illuminate\Database\Eloquent\Model;
+
+class Enfermedad extends Model
+{
+    use HasFactory;
+
+    public function signos()
+    {
+        return $this->belongsToMany(Signo::class);
+    }
+
+    public function sintomas()
+    {
+        return $this->belongsToMany(Sintoma::class);
+    }
+
+    public function pruebaLaboratorio()
+    {
+        return $this->belongsTo(PruebaLaboratorio::class);
+    }
+
+    public function tratamiento()
+    {
+        return $this->belongsTo(Tratamiento::class);
+    }
+}
---
+++ app/Models/Paciente.php
@@ -0,0 +1,21 @@
+<?php
+
+namespace App\Models;
+
+use Illuminate\Database\Eloquent\Factories\HasFactory;
+use Illuminate\Database\Eloquent\Model;
+
+class Paciente extends Model
+{
+    use HasFactory;
+
+    public function estado()
+    {
+        return $this->belongsTo(Estado::class);
+    }
+
+    public function citas()
+    {
+        return $this->hasMany(Cita::class);
+    }
+}
